f2702c4bd7f24dbb8d16763bd30d1fbd8012833f
danielhinsching <danielsantoshinsching@gmail.com>
2024-09-17 22:39:47 -0300

    reorganização responsividade menu superior

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,17 @@
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from 'vue';
-import ListagemProdutos from '@/components/ListagemProdutos.vue';
-import MenuSuperiorXl from './components/MenuSuperiorXl.vue';
-import MenuSuperiorLg from './components/MenuSuperiorLg.vue';
-import MenuSuperiorMd from './components/MenuSuperiorMd.vue';
-import MenuSuperiorSm from './components/MenuSuperiorSm.vue';
-import MenuSuperiorXs from './components/MenuSuperiorXs.vue';
+  import { useMonitor } from '@/composables/monitor';
 
-const currentMenu = ref(MenuSuperiorXl);
-
-const updateMenuSuperior = () => {
-  const width = window.innerWidth;
-  if (width <= 768) {
-    currentMenu.value = MenuSuperiorXs;
-  } else if (width <= 992) {
-    currentMenu.value = MenuSuperiorSm;
-  } else if (width <= 1200) {
-    currentMenu.value = MenuSuperiorMd;
-  } else if (width <= 1500) {
-    currentMenu.value = MenuSuperiorLg;
-  } else {
-    currentMenu.value = MenuSuperiorXl;
-  }
-};
-
-onMounted(() => {
-  updateMenuSuperior();
-  window.addEventListener('resize', updateMenuSuperior);
-});
-
-onBeforeUnmount(() => {
-  window.removeEventListener('resize', updateMenuSuperior);
-});
+  const { menu } = useMonitor();
 </script>
 
 <template>
-  <component :is="currentMenu" />
-  <ListagemProdutos />
+  <div>
+    <component :is="menu" />
+    <main>
+      <router-view />
+    </main>
+    <footer>
+      <p>Copyright &copy; 2024</p>
+    </footer>
+  </div>
 </template>
