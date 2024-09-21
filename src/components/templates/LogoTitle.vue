127d42822fb9c7a0ec41a643c5c9c6371038d6e2
danielhinsching <danielsantoshinsching@gmail.com>
2024-09-21 18:09:17 -0300

    Ajustes Header,Footer e Aside; Preparação incial página de Login

---
+++ src/components/templates/LogoTitle.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+<template>
+  <div class="logo">
+    <img src="@/assets/logoCo-off.png" alt="Co-Off Logo" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  display: flex;
+  padding-left: 3rem;
+  gap: 1rem;
+  align-items: center;
+}
+
+.logo img {
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+.logo h2 {
+  font-family: 'Belleza', sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
+</style>
