aaaef7fd952728925c5f78f78777fe7c0ea2a1d7
danielhinsching <danielsantoshinsching@gmail.com>
2024-10-01 14:05:13 -0300

    alteraçõea header erro

--- src/components/templates/large/LargeHeader.vue
+++ src/components/templates/large/LargeHeader.vue
@@ -1,14 +1,6 @@
 <script setup></script>
 
 <template>
-<<<<<<< HEAD
-<div class="box-header">
-  <div class="logo"></div>
-  <div class="icons">
-    <i class="mdi mdi-magnify" />
-    <i class="mdi mdi-account-circle-outline" />
-    <i class="mdi mdi-cart-outline" />
-=======
   <div class="flex-container">
       <div/>
       <div/>
@@ -23,15 +15,11 @@
           <Account class="icon-login"/>
         </button>
       </div>
->>>>>>> ba6c45c (alteraçẽs finais Large Header)
   </div>
   <div class="login-button"></div>
-</div>
 </template>
 
 <style scoped>
-<<<<<<< HEAD
-=======
 .button {
   display: flex;
   background-color: #000000;
@@ -69,58 +57,13 @@
   height: fit-content;
   justify-content: center;
 }
->>>>>>> ba6c45c (alteraçẽs finais Large Header)
 .icons {
   display: flex;
   gap: 1rem;
   align-items: center;
-<<<<<<< HEAD
-  font-size: 1.7rem;
-}
-.logo {
-  position: absolute;
-width: 319px;
-height: 87px;
-left: 561px;
-top: -7px;
-
-background: url(Co-Off-removebg-preview.png);
-
-}
-
-
-.box-header {
-
-box-sizing: border-box;
-
-position: absolute;
-width: 100px;
-height: 32px;
-left: 1306px;
-top: 20px;
-
-border: 1px solid #FFFFFF;
-border-radius: 100px;
-
-}
-/* Login */
-.login-button {
-position: absolute;
-width: 70px;
-height: 32px;
-left: 1306px;
-top: 20px;
-
-font-family: 'Blinker';
-font-style: normal;
-font-weight: 300;
-font-size: 15px;
-line-height: 15px;
-=======
   font-size: 1rem;
   margin-left: auto;
   color: #ffffff;
->>>>>>> ba6c45c (alteraçẽs finais Large Header)
 }
 
 
